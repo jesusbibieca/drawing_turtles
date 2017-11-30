d8fe7e171af49b3818b188113dc3439a67e18ce8
Jesus Bibieca <jesusbibieca52@gmail.com>
2017-11-30 11:12:44 -0400

    refactor draw sqr function

--- drawing.py
+++ drawing.py
@@ -1,21 +1,20 @@
 import turtle
 
 def draw_square():
-	window = turtle.Screen()
-	window.bgcolor("red")
+    window = turtle.Screen()
+    window.bgcolor("yellow")
 
-	brad = turtle.Turtle()
-	brad.shape("turtle")
-	brad.speed(2)
-
-	brad.forward(100)
-	brad.right(90)
-	brad.forward(100)
-	brad.right(90)
-	brad.forward(100)
-	brad.right(90)
-	brad.forward(100)
-	brad.right(90)
-	window.exitonclick()
+    brad = turtle.Turtle()
+    brad.shape("turtle")
+    brad.color("blue")
+    brad.speed(10)
+    
+    for i in range (1,37):
+        brad.right(10)
+        for x in range (1, 5):
+            brad.forward(100)
+            brad.right(90)
+       
+    window.exitonclick()
 
 draw_square()
